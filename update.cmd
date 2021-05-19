ef57ae624d674aadc09cb14dccd6c10acbf2a7d3
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-05-19 18:08:03 +0100

    Added update command scripts.

---
+++ update.cmd
@@ -0,0 +1,8 @@
+echo "Updating code base."
+git fetch origin --prune
+git submodule init
+git submodule update
+echo "Code base up to date."
+echo "Updating requirements"
+pip3 install -r requirements.txt
+echo "Requirements up to date."
